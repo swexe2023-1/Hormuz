9c1ee04eb7b7fd185cd1ba69f813a2307427c456
Syo Kondo <2111140067e@ed.fuk.kindai.ac.jp>
2023-12-06 09:01:04 +0000

    なぜ再帰呼び出しをしようとしているのだろうか

--- app/controllers/serch_product_controller.rb
+++ app/controllers/serch_product_controller.rb
@@ -1,4 +1,34 @@
 class SerchProductController < ApplicationController
     def serch
     end
+    
+    def category_view
+        @all_major=Major.all
+        if(params[:nc]!=nil && @all_major.find_by(jid: params[:nc])==nil)
+            Major.create(jid: params[:nc])
+            redirect_to '/serch_product/category_view'
+        end
+    end
+    
+    def category_all_delete
+        Major.find(params[:m_id]).destroy
+        redirect_to '/serch_product/category_view'
+    end
+    
+    def regist_category
+        if @major==nil && params[:major_id]==nil
+        @major=Major.find(params[:m_id])
+        end
+        
+        if @major==nil
+           @major=Major.find(params[:major_id])
+        end
+        @middle=Middle.where(major_id: @major)
+        
+        if(@middle.find_by(did: params[:mc])==nil)
+        Middle.create(did: params[:mc],up_middle_id: params[:um],major_id: params[:major_id])
+        end
+        
+        #redirect_to '/serch_product/regist_category'
+    end
 end
